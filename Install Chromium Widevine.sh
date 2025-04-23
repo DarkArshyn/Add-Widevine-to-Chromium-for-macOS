a4aa39b11f32677faad4b0c3768e8e7044e30893
DarkArshyn <8863727+DarkArshyn@users.noreply.github.com>
2025-04-23 16:21:04 +0200

    Update Install Chromium Widevine.sh
    Replacement of wget with curl
    Fixed an error when downloading the file if it already exists
    Execution of the copy using sudo to avoid permission issues

--- Install Chromium Widevine.sh
+++ Install Chromium Widevine.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-cd /tmp/
-wget https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg
-hdiutil attach ./googlechrome.dmg -nobrowse
-cp -R /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/Frameworks/Google\ Chrome\ Framework.framework/Libraries/WidevineCdm /Applications/Chromium.app/Contents/Frameworks/Chromium\ Framework.framework/Libraries/WidevineCdm
+[ ! -f /tmp/googlechrome.dmg ] && curl -L https://dl.google.com/chrome/mac/universal/stable/GGRO/googlechrome.dmg -o /tmp/googlechrome.dmg
+hdiutil attach /tmp/googlechrome.dmg -nobrowse
+sudo cp -R /Volumes/Google\ Chrome/Google\ Chrome.app/Contents/Frameworks/Google\ Chrome\ Framework.framework/Libraries/WidevineCdm /Applications/Chromium.app/Contents/Frameworks/Chromium\ Framework.framework/Libraries/WidevineCdm
 hdiutil detach /Volumes/Google\ Chrome
